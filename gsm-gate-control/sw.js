27d7c52a65895a31e2cc065551ccbb44dc0b4903
Karan <44883947+mrmaligi@users.noreply.github.com>
2025-03-04 13:38:15 +1100

    Add files via upload

---
+++ gsm-gate-control/sw.js
@@ -0,0 +1,67 @@
+const CACHE_NAME = "gsm-gate-control-v1"
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/icons/icon-72x72.png",
+  "/icons/icon-96x96.png",
+  "/icons/icon-128x128.png",
+  "/icons/icon-144x144.png",
+  "/icons/icon-152x152.png",
+  "/icons/icon-192x192.png",
+  "/icons/icon-384x384.png",
+  "/icons/icon-512x512.png",
+]
+
+// Install service worker
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache)
+    }),
+  )
+})
+
+// Activate and cleanup old caches
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((cacheName) => {
+            return cacheName !== CACHE_NAME
+          })
+          .map((cacheName) => {
+            return caches.delete(cacheName)
+          }),
+      )
+    }),
+  )
+})
+
+// Serve cached content when offline
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      if (response) {
+        return response
+      }
+      return fetch(event.request).then((response) => {
+        // Don't cache if not a valid response
+        if (!response || response.status !== 200 || response.type !== "basic") {
+          return response
+        }
+
+        // Clone the response
+        const responseToCache = response.clone()
+
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, responseToCache)
+        })
+
+        return response
+      })
+    }),
+  )
+})
+
